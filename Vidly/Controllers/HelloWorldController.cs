c46ff19ce75bed3de40899e7a6c92d2f9353d0ee
Afahd Ali <afahd.ali@valtech.com>
2019-04-14 18:16:17 +0200

    First changes -> added example controller and corresponding views.

---
+++ Vidly/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+		//Get: /HelloWorld/
+        public IActionResult Index()
+        {
+	        return View();
+        }
+
+	    //Get: /HelloWorld/Welcome/
+	    public string Welcome(string name, int ID = 1)
+	    {
+		    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+	    }
+    }
+}
